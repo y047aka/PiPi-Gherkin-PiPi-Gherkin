521b1262d97d367ab6f194fd62e3cab71e6d3426
Yoshitaka Totsuka <y047aka@gmail.com>
2022-05-03 10:58:49 +0900

    Works on Rustykeys

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
 
+use core::convert::Infallible;
+
+use cortex_m::{asm, prelude::*};
 use cortex_m_rt::entry;
-use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::fixed_point::FixedPoint;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_time::duration::Extensions as _;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
@@ -14,32 +16,29 @@ use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
+    self, clocks::init_clocks_and_plls, pac, sio::Sio, usb::UsbBus, watchdog::Watchdog,
 };
 
-/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
-/// if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+use usb_device as usbd;
+use usbd::{
+    class_prelude::UsbBusAllocator,
+    device::{UsbDeviceBuilder, UsbVidPid},
+};
 
-/// Our Cortex-M systick goes from this value down to zero. For our timer maths
-/// to work, this value must be of the form `2**N - 1`.
-const SYSTICK_RELOAD: u32 = 0x00FF_FFFF;
+use usbd_hid::{
+    descriptor::{KeyboardReport, SerializedDescriptor},
+    hid_class::{
+        HIDClass, HidClassSettings, HidCountryCode, HidProtocol, HidSubClass, ProtocolModeConfig,
+    },
+};
 
 #[entry]
 fn main() -> ! {
-    info!("Program start");
-
     let mut pac = pac::Peripherals::take().unwrap();
-    let mut core = pac::CorePeripherals::take().unwrap();
 
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
-
     let clocks = init_clocks_and_plls(
-        XTAL_FREQ_HZ,
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -50,6 +49,35 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+
+    let bus = UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    );
+    let bus_allocator = UsbBusAllocator::new(bus);
+    let vid_pid = UsbVidPid(0x6666, 0x0470);
+    let mut hid = HIDClass::new_with_settings(
+        &bus_allocator,
+        KeyboardReport::desc(),
+        10,
+        HidClassSettings {
+            subclass: HidSubClass::NoSubClass,
+            protocol: HidProtocol::Keyboard,
+            config: ProtocolModeConfig::ForceReport,
+            locale: HidCountryCode::NotSupported,
+        },
+    );
+    let mut dev = UsbDeviceBuilder::new(&bus_allocator, vid_pid)
+        .manufacturer("y047aka")
+        .product("PiPi-Gherkin-PiPi-Gherkin")
+        .serial_number("001")
+        .build();
+
+    let sio = Sio::new(pac.SIO);
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -57,26 +85,51 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Use systick as a count-down timer
-    core.SYST.set_reload(SYSTICK_RELOAD);
-    core.SYST.clear_current();
-    core.SYST.enable_counter();
+    let col0 = pins.gpio2.into_pull_up_input();
+    let col1 = pins.gpio3.into_pull_up_input();
+    let col2 = pins.gpio4.into_pull_up_input();
+    let col3 = pins.gpio5.into_pull_up_input();
+    let col4 = pins.gpio6.into_pull_up_input();
+    let col5 = pins.gpio7.into_pull_up_input();
+    let mut row0 = pins.gpio8.into_push_pull_output();
+    let mut row1 = pins.gpio9.into_push_pull_output();
+    let mut row2 = pins.gpio10.into_push_pull_output();
+    let mut row3 = pins.gpio11.into_push_pull_output();
+    let mut row4 = pins.gpio12.into_push_pull_output();
+    let mut row5 = pins.gpio13.into_push_pull_output();
+    let mut row6 = pins.gpio14.into_push_pull_output();
+    let mut row7 = pins.gpio15.into_push_pull_output();
+    let mut row8 = pins.gpio16.into_push_pull_output();
+    let mut row9 = pins.gpio17.into_push_pull_output();
+    let mut row10 = pins.gpio18.into_push_pull_output();
+    let cols: &[Column] = &[&col0, &col1, &col2, &col3, &col4, &col5];
+    let rows: &mut [Row] = &mut [
+        &mut row0, &mut row1, &mut row2, &mut row3, &mut row4, &mut row5, &mut row6, &mut row7,
+        &mut row8, &mut row9, &mut row10,
+    ];
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    let mut scan_countdown = timer.count_down();
+    scan_countdown.start(10.milliseconds());
 
     let mut led_pin = pins.led.into_push_pull_output();
 
-    for _ in 0..5 {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
-    }
-    // Now just spin (whilst the UART does its thing)
-    for _ in 0..1_000_000 {
-        cortex_m::asm::nop();
+    for i in 1..100_000 {
+        dev.poll(&mut [&mut hid]);
+
+        if scan_countdown.wait().is_ok() {
+            let state = scan_keys(rows, cols);
+            let report = build_report(&state);
+            hid.push_input(&report).ok();
+        }
+        // drop received data
+        hid.pull_raw_output(&mut [0; 64]).ok();
+
+        if i % 5_000 == 0 {
+            led_pin.set_high().unwrap();
+        }
+        if i % 10_000 == 0 {
+            led_pin.set_low().unwrap();
+        }
     }
 
     // Reboot back into USB mode (no activity, both interfaces enabled)
@@ -87,5 +140,83 @@ fn main() -> ! {
         cortex_m::asm::nop();
     }
 }
+
+pub type Column<'a> = &'a dyn InputPin<Error = Infallible>;
+pub type Row<'a> = &'a mut dyn OutputPin<Error = Infallible>;
+pub type StateMatrix = [[bool; 3]; 2];
+
+fn scan_keys(rows: &mut [Row], cols: &[Column]) -> StateMatrix {
+    let mut matrix = [[false; 3]; 2];
+    for (row_pin, row_state) in rows.iter_mut().zip(matrix.iter_mut()) {
+        row_pin.set_low().unwrap();
+        asm::delay(10);
+        for (col_pin, col_state) in cols.iter().zip(row_state.iter_mut()) {
+            *col_state = col_pin.is_low().unwrap();
+        }
+        row_pin.set_high().unwrap();
+        asm::delay(10);
+    }
+    matrix
+}
+
+fn build_report(matrix: &StateMatrix) -> KeyboardReport {
+    let mut keycodes = [0u8; 6];
+    let mut keycode_count = 0;
+    let mut push_key = |keycode: u8| {
+        keycodes[keycode_count] = keycode;
+        keycode_count += 1;
+    };
+    let mut modifier = 0;
+
+    if matrix[0][2] {
+        modifier = 0x02; // LEFT SHIFFT
+    } else if matrix[0][0] {
+        push_key(KC_K);
+
+        // Reboot back into USB mode (no activity, both interfaces enabled)
+        bsp::hal::rom_data::reset_to_usb_boot(0, 0);
+    } else if matrix[0][1] {
+        push_key(KC_O);
+    } else if matrix[1][0] {
+        push_key(KC_B);
+    } else if matrix[1][1] {
+        push_key(KC_A);
+    }
+
+    KeyboardReport {
+        modifier,
+        reserved: 0,
+        leds: 0,
+        keycodes,
+    }
+}
+
+const KC_NO: u8 = 0x00; // Reserved (no event indicated)
+const KC_A: u8 = 0x04; // a and A
+const KC_B: u8 = 0x05; // b and B
+const KC_C: u8 = 0x06; // c and C
+const KC_D: u8 = 0x07; // d and D
+const KC_E: u8 = 0x08; // e and E
+const KC_F: u8 = 0x09; // f and F
+const KC_G: u8 = 0x0A; // g and G
+const KC_H: u8 = 0x0B; // h and H
+const KC_I: u8 = 0x0C; // i and I
+const KC_J: u8 = 0x0D; // j and J
+const KC_K: u8 = 0x0E; // k and K
+const KC_L: u8 = 0x0F; // l and L
+const KC_M: u8 = 0x10; // m and M
+const KC_N: u8 = 0x11; // n and N
+const KC_O: u8 = 0x12; // o and O
+const KC_P: u8 = 0x13; // p and P
+const KC_Q: u8 = 0x14; // q and Q
+const KC_R: u8 = 0x15; // r and R
+const KC_S: u8 = 0x16; // s and S
+const KC_T: u8 = 0x17; // t and T
+const KC_U: u8 = 0x18; // u and U
+const KC_V: u8 = 0x19; // v and V
+const KC_W: u8 = 0x1A; // w and W
+const KC_X: u8 = 0x1B; // x and X
+const KC_Y: u8 = 0x1C; // y and Y
+const KC_Z: u8 = 0x1D; // z and Z
 
 // End of file
